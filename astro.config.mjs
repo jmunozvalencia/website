06443a4a9810c0b45b803cd5035590e4c3e58f05
moneyba <moneybaroger@gmail.com>
2022-09-15 19:38:49 +0100

    Add missing files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [embeds()],
+  legacy: {
+    // Example: Add support for legacy Markdown features
+    astroFlavoredMarkdown: true,
+  },
 });
