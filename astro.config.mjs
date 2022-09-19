2050c187c4dde968aeca8fa47dcb6130777cca6c
moneyba <moneybaroger@gmail.com>
2022-09-19 20:00:50 +0100

    Add collaborate section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image()],
   legacy: {
     // Example: Add support for legacy Markdown features
-    astroFlavoredMarkdown: true,
+    astroFlavoredMarkdown: true
   },
+  integrations: [tailwind(), image()]
 });
