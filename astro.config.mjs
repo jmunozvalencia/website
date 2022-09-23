b41297e9d6246a3bd8b9add6b5c90ff19fc2726d
moneyba <moneybaroger@gmail.com>
2022-09-23 22:02:45 +0100

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  legacy: {
-    // Example: Add support for legacy Markdown features
-    astroFlavoredMarkdown: true
-  },
-  integrations: [image()]
+    legacy: {
+        // Example: Add support for legacy Markdown features
+        astroFlavoredMarkdown: true
+    },
+    integrations: [tailwind(), image()]
 });
