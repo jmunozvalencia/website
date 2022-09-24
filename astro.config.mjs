ed982b597f8b43efeab7386699eb75d0bfdf81b0
moneyba <moneybaroger@gmail.com>
2022-09-24 10:50:59 +0100

    Fix collaborate table

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,4 +10,9 @@ export default defineConfig({
         astroFlavoredMarkdown: true
     },
     integrations: [tailwind(), image()],
+    vite: {
+        ssr: {
+            external: ["svgo"],
+        },
+    },
 });
